432f712bdf81d8a631d6162873590f00a1ba6ddc
Andy Duong <andydng@hotmail.com>
2015-03-05 17:37:09 -0500

    Added barchart

--- js/script.js
+++ js/script.js
@@ -13,66 +13,104 @@ $(function() {
   });
 });
 
+/* BARGRAPH of BARLEY */
+
+/*    
+var data = {
+    labels: [ "Russia", 
+              "Ukraine", 
+              "France", 
+              "Germany", 
+              "Spain", 
+              "Australia", 
+              "Canada",
+              "Turkey",
+              "United Kingdom",
+              "Argentina"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [ 16938000, 
+                    9097700, 
+                    8775000, 
+                    8733800, 
+                    8328200, 
+                    7994720, 
+                    7755700, 
+                    7600000, 
+                    5494000, 
+                    4076940]
+        }]};
+
+        window.onload = function(){
+  var canv = document.getElementById("barley-chart").getContext("2d");
+  window.myBarChart = new Chart(canv).Bar(data, {responsive:true});
+};*/
+
 /* DOUGHNUT of HOPS*/
 
 var doughnutData = [
         {
           value: 34434,
-          color:"#D52B1E",
+          color:"#245239",
           highlight: "#903000",
           label: "Germany"
         },
         {
           value: 27782,
-          color: "#0052A5",
+          color: "#306343",
           highlight: "#9A3E00",
           label: "United States"
         },
         {
           value: 21792,
-          color: "#009a44",
+          color: "#3B744C",
           highlight: "#A34D00",
           label: "Ethiopia"
         },
         {
           value: 11000,
-          color: "#FF0000",
+          color: "#478556",
           highlight: "#AD5B00",
           label: "China"
         },
         {
           value: 4338,
-          color: "#245239",
+          color: "#52965F",
           highlight: "#B66A00",
           label: "Czech Republic"
         },
         {
           value: 2206,
-          color: "#f8f8f8",
+          color: "#5EA769",
           highlight: "#C07800",
           label: "Poland"
         },
         {
           value: 2000,
-          color: "#C60C30",
-          highlight: "#ff2200",
+          color: "#6AB772",
+          highlight: "#C07800",
           label: "North Korea"
         },
         {
           value: 1650,
-          color: "#348064",
+          color: "#75C87C",
           highlight: "#D39500",
           label: "Albania"
         },
         {
           value: 1650,
-          color: "#959234",
+          color: "#81D985",
           highlight: "#DDA300",
           label: "United Kingdom"
         },
         {
           value: 1338,
-          color: "#q53213",
+          color: "#8CEA8F",
           highlight: "#E6B200",
           label: "Slovenia"
         },
@@ -83,3 +121,39 @@ var doughnutData = [
         var ctx = document.getElementById("chart-area").getContext("2d");
         window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
       };
+
+function createChart() {
+  data = {
+      labels: [ "Russia", 
+                "Ukraine", 
+                "France", 
+                "Germany", 
+                "Spain", 
+                "Australia", 
+                "Canada",
+                "Turkey",
+                "United Kingdom",
+                "Argentina"],
+      datasets: [
+          {
+              label: "My First dataset",
+              fillColor: "rgba(220,220,220,0.5)",
+              strokeColor: "rgba(220,220,220,0.8)",
+              highlightFill: "rgba(220,220,220,0.75)",
+              highlightStroke: "rgba(220,220,220,1)",
+              data: [ 16938000, 
+                      9097700, 
+                      8775000, 
+                      8733800, 
+                      8328200, 
+                      7994720, 
+                      7755700, 
+                      7600000, 
+                      5494000, 
+                      4076940]
+          }]}
+  var cht = document.getElementById('barley-chart');
+  var ctx = cht.getContext('2d');
+  var barChart = new Chart(ctx).Bar(data,{responsive : true});
+}
+window.onload = createChart;
